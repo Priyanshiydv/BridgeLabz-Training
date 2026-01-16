babb580691e1fbc869e9aba6bd45d0797f58cb90
Priyanshi Yadav <priyanshiydv2100@gmail.com>
2026-01-16 11:32:10 +0530

    [Priyanshi Yadav].Add dsa scenario problem

---
+++ dsa-csharp-practice/scenario-based/BrowserBuddy/BrowserBuddy/BrowserBuddyMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BrowserBuddy
+//15-01-2026
+{
+    internal class BrowserBuddyMain
+    {
+        static void Main(string[] args)
+        {
+            BrowserBuddyMenu menu = new BrowserBuddyMenu();
+            menu.Start();
+        }
+    }
+}
